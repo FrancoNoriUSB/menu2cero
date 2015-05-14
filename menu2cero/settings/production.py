eee8b7d25a4d05fd8531e1f9bd6447661fac83dd
Franco Nori <francong2@gmail.com>
2015-05-13 21:39:32 -0430

    Platos con su acordion y agregar mas de un plato en admin

--- menu2cero/settings/production.py
+++ menu2cero/settings/production.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'menu2cero.urls'
 
 WSGI_APPLICATION = 'menu2cero.wsgi.application'
 
-LOGIN_URL = '/admin/login/'
+LOGIN_URL = 'pre-home/'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
